28403096a92c14683df2e494a3fd7c1b45810264
Ashley Claiborne <languageisaliquid@gmail.com>
2020-03-21 10:56:49 -0500

    Add project files.

---
+++ SquaredRandoms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SquaredRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random _rng = new Random();
+
+            var randoms = new List<int>();
+            var squaredRandoms = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                randoms.Add(_rng.Next(1, 50));
+            }
+
+            foreach (var num in randoms)
+            {
+                squaredRandoms.Add(num * num);
+            }
+
+            var oddSquaredRandoms = squaredRandoms.Where(square => (square % 2 != 0)).ToList();
+
+            foreach (var oddNum in oddSquaredRandoms)
+            {
+                squaredRandoms.Remove(oddNum);
+            }
+        }
+    }
+}
